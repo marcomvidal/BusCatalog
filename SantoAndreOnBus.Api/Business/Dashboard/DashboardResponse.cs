dd4e4579d6c793d65c812f4134f3e11e7a131aa5
Marco M. Vidal <marcomvidal@gmail.com>
2023-12-16 00:12:07 -0300

    feature: remodelling of companies and dashboard concepts

--- SantoAndreOnBus.Api/Business/Dashboard/DashboardResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace SantoAndreOnBus.Api.Business.Dashboard;
-
-public class DashboardResponse
-{
-    public int Companies { get; set; }
-    public int Lines { get; set; }
-    public int Vehicles { get; set; }
-}
